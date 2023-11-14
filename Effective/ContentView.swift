153ae4fce0dbd2f431ff8f892ae83723b3a297c2
Kirill Zykov <kiryushaz.gaming@gmail.com>
2023-11-14 23:42:57 +0600

    implemented 3rd state

--- Effective/ContentView.swift
+++ Effective/ContentView.swift
@@ -11,28 +11,36 @@ struct ContentView: View {
     var body: some View {
         VStack (spacing: 32) {
             HeaderView(name: "James", avatar: "avatar")
-            VStack (spacing: 20) {
-                MainCardView(
-                    avatar: "doctor1",
-                    name: "Dr. Imran Syahir",
-                    title: "General Doctor",
-                    date: "Sunday, 12 June",
-                    time: "11:00 - 12:00 AM"
-                )
-                HStack (alignment: .center, spacing: 12) {
-                    Image(systemName: "magnifyingglass")
-                        .foregroundColor(Color(red: 0.53, green: 0.59, blue: 0.73))
-                        .frame(width: 24, height: 24)
-                    Text("Search doctor or health issue")
-                        .font(Font.custom("Poppins-Regular", size: 15))
-                        .foregroundColor(Color(red: 0.53, green: 0.59, blue: 0.73))
+            VStack (spacing: 24) {
+                VStack (spacing: 20) {
+                    MainCardView(
+                        avatar: "doctor1",
+                        name: "Dr. Imran Syahir",
+                        title: "General Doctor",
+                        date: "Sunday, 12 June",
+                        time: "11:00 - 12:00 AM"
+                    )
+                    HStack (alignment: .center, spacing: 12) {
+                        Image(systemName: "magnifyingglass")
+                            .foregroundColor(Color(red: 0.53, green: 0.59, blue: 0.73))
+                            .frame(width: 24, height: 24)
+                        Text("Search doctor or health issue")
+                            .font(Font.custom("Poppins-Regular", size: 15))
+                            .foregroundColor(Color(red: 0.53, green: 0.59, blue: 0.73))
+                    }
+                    .padding(16)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .background(Color(red: 0.98, green: 0.98, blue: 0.98))
+                    .cornerRadius(12)
                 }
-                .padding(16)
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .background(Color(red: 0.98, green: 0.98, blue: 0.98))
-                .cornerRadius(12)
+                HStack (alignment: .top, spacing: 13) {
+                    SectionView(icon: "snowflake", text: "Covid 19")
+                    SectionView(icon: "person.fill.badge.plus", text: "Doctor")
+                    SectionView(icon: "link", text: "Medicine")
+                    SectionView(icon: "stethoscope", text: "Hospital")
+                }
             }
-            
+                        
         }
         .padding(24)
         .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
