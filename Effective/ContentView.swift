587e2744bb1e1c98f5001c0f00a05e07bfabf2b5
Kirill Zykov <kiryushaz.gaming@gmail.com>
2023-11-14 23:19:22 +0600

    2nd state: added search field

--- Effective/ContentView.swift
+++ Effective/ContentView.swift
@@ -11,15 +11,31 @@ struct ContentView: View {
     var body: some View {
         VStack (spacing: 32) {
             HeaderView(name: "James", avatar: "avatar")
-            MainCardView(
-                avatar: "doctor1",
-                name: "Dr. Imran Syahir",
-                title: "General Doctor",
-                date: "Sunday, 12 June",
-                time: "11:00 - 12:00 AM"
-            )
+            VStack (spacing: 20) {
+                MainCardView(
+                    avatar: "doctor1",
+                    name: "Dr. Imran Syahir",
+                    title: "General Doctor",
+                    date: "Sunday, 12 June",
+                    time: "11:00 - 12:00 AM"
+                )
+                HStack (alignment: .center, spacing: 12) {
+                    Image(systemName: "magnifyingglass")
+                        .foregroundColor(Color(red: 0.53, green: 0.59, blue: 0.73))
+                        .frame(width: 24, height: 24)
+                    Text("Search doctor or health issue")
+                        .font(Font.custom("Poppins-Regular", size: 15))
+                        .foregroundColor(Color(red: 0.53, green: 0.59, blue: 0.73))
+                }
+                .padding(16)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .background(Color(red: 0.98, green: 0.98, blue: 0.98))
+                .cornerRadius(12)
+            }
+            
         }
         .padding(24)
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
     }
 }
 
