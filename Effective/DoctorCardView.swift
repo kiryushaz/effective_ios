80d3c7f186ef237f08bfd9168a9d30be794025a5
Kirill Zykov <kiryushaz.gaming@gmail.com>
2023-11-15 00:17:36 +0600

    4th state screen: added near doctor list, screen is now scrollable

--- Effective/DoctorCardView.swift
+++ Effective/DoctorCardView.swift
@@ -1,5 +1,5 @@
 //
-//  MainCardView.swift
+//  DoctorCardView.swift
 //  Effective
 //
 //  Created by  User on 14.11.2023.
@@ -7,15 +7,17 @@
 
 import SwiftUI
 
-struct MainCardView: View {
+struct DoctorCardView: View {
     let avatar: String
     let name: String
     let title: String
-    let date: String
+    let distance: String
+    let avg_rating: String
+    let cnt_ratings: String
     let time: String
     
     var body: some View {
-        VStack (alignment: .center, spacing: 16) {
+        VStack (alignment: .center, spacing: 20) {
             HStack (alignment: .center, spacing: 12) {
                 ZStack {
                     Rectangle()
@@ -29,41 +31,46 @@ struct MainCardView: View {
                 VStack (alignment: .leading, spacing: 8) {
                     Text(name)
                         .font(Font.custom("Poppins-Bold", size: 16))
-                        .foregroundColor(.white)
                         .fontWeight(.bold)
                     Text(title)
                         .font(Font.custom("Poppins-Regular", size: 12))
-                        .foregroundColor(.white)
+                        .foregroundColor(Color(red: 0.53, green: 0.59, blue: 0.73))
                 }
                 Spacer()
-                Image(systemName: "chevron.right")
-                    .foregroundColor(.white)
+                HStack (spacing: 8) {
+                    Image(systemName: "location.north.fill")
+                        .resizable()
+                        .frame(width: 16, height: 16)
+                    Text(distance)
+                        .font(Font.custom("Poppins-Regular", size: 14))
+                }.foregroundColor(Color(red: 0.53, green: 0.59, blue: 0.73))
             }
             .padding(0)
             .frame(maxWidth: .infinity, alignment: .center)
             Divider().background(.white)
             HStack (alignment: .top, spacing: 32) {
                 HStack (spacing: 8) {
-                    Image(systemName: "calendar")
+                    Image(systemName: "star.fill")
                         .resizable()
                         .frame(width: 16, height: 16)
-                    Text(date)
+                    Text("\(avg_rating) (\(cnt_ratings) Reviews)")
                         .font(Font.custom("Poppins-Regular", size: 12))
-                }
+                }.foregroundColor(Color(red: 1, green: 0.69, blue: 0.32))
                 HStack (spacing: 8) {
                     Image(systemName: "clock")
                         .resizable()
                         .frame(width: 16, height: 16)
-                    Text(time)
+                    Text("Open at \(time)")
                         .font(Font.custom("Poppins-Regular", size: 12))
-                }
+                }.foregroundColor(Color(red: 0.28, green: 0.58, blue: 1))
             }
             .foregroundColor(.white)
             .frame(maxWidth: .infinity, alignment: .topLeading)
         }
-        .padding(20)
-        .frame(maxWidth: .infinity)
-        .background(Color(red: 0.28, green: 0.58, blue: 1))
+        .padding(.horizontal, 16)
+        .padding(.vertical, 20)
+        .background(.white)
         .cornerRadius(12)
+        .shadow(color: Color(red: 0.35, green: 0.46, blue: 0.65).opacity(0.04), radius: 10, x: 2, y: 12)
     }
 }
