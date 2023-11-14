50ae47b2c07a8df0025905d41954e67e21176ed4
Kirill Zykov <kiryushaz.gaming@gmail.com>
2023-11-14 15:59:08 +0300

    Initial Commit

---
+++ Effective/EffectiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  EffectiveApp.swift
+//  Effective
+//
+//  Created by  User on 14.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct EffectiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
